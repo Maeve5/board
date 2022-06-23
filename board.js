9dbd515432604a894f3563054b70cd9f43fdc26e
Maeve5 <107019969+Maeve5@users.noreply.github.com>
2022-06-23 18:01:52 +0900

    Add files via upload
    파일간 데이터 주고받을때 쓰는거 뭐...? import, export? what.,,.

--- board.js
+++ board.js
@@ -1,22 +1,71 @@
-const submit = document.getElementById("submit");
-const cancle = document.getElementById("cancle");
+
+// function setValueChange() {
+//     const titleValue = document.getElementById("n_title").value;
+//     console.log(titleValue);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+export {setValueChange, } 
+
+const num = 0;
+
+const today = new Date();
+const year = today.getFullYear();
+const month = ('0' + (today.getMonth() + 1));
+const day = today.getDate();
+const date = year + '.' + month + '.' + day;
+
+const count = 0;
 
 function setValueChange(){
-    var title = document.getElementById('n_title').value;
-    var writer = document.getElementById('n_writer').value;
-    var password = document.getElementById('n_password').value;
-    var cont = document.getElementById('n_cont').value;  
     
-    /*객체형일 경우를 대비하여 JSON 쓸 필요 있어보임*/
+    const title = document.getElementById('n_title').value;
+    const writer = document.getElementById('n_writer').value;
+    const password = document.getElementById('n_password').value;
+    const cont = document.getElementById('n_cont').value;  
+    
+    const notice = [
+        {name:"num",value:num},
+        {name:"title",value:title},
+        {name:"writer",value:writer},
+        {name:"date",value:date},
+        {name:"count",value:count},
+        {name:"cont",value:cont}
+    ];
+    
+    // new Array();
+    // notice[0] = num;
+    // notice[1] = title;
+    // notice[2] = writer;
+    // notice[3] = date;
+    // notice[4] = count;
+    // notice[5] = cont;
+    const submit = document.getElementById("submit");
+    
+    window.localStorage.setItem("notice",JSON.stringify(notice));
+    window.localStorage.setItem("password",password);
 
-    if(submit) {/*등록을 누르면 로컬스토리지 올라가게 else 취소 누르면 데이터 삭제*/
-        window.localStorage.setItem("title",title);
-        window.localStorage.setItem("writer",writer);
-        window.localStorage.setItem("password",password);
-        window.localStorage.setItem("cont",cont);
-    }
+    /*객체형인 배열형태로 데이터를 저장하기 위해 JSON 사용*/
 }
 
-if(cancle) {
-    window.localStorage.removeItem();
-}
+
+//     if(submit) {/*등록을 누르면 로컬스토리지 올라가게 else 취소 누르면 데이터 삭제*/
+//         window.localStorage.setItem("title",title);
+//         window.localStorage.setItem("writer",writer);
+//         window.localStorage.setItem("cont",cont);
+//     }
+
+// if(cancle) {
+//     window.localStorage.removeItem();
+// }
